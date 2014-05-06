9a40bef56d1748ed141e1e13355f6df572d2ad7a
Yasser Moradi <y.moradi@outlook.com>
2014-05-06 14:05:16 +0430

    Add initial version of SubtitleHelper.cs

---
+++ SubtitleHelper.cs
@@ -0,0 +1,29 @@
+public static class SubtitleHelper
+    {
+        /// <summary>
+        /// Convert WebVTT subtitles to Srt subtitles
+        /// </summary>
+        /// <param name="webvttContent">WebVTT string</param>
+        /// <returns>SRT result</returns>
+        public static String ConvertWebvttToSrt(String webvttContent)
+        {
+            if (webvttContent == null)
+                throw new ArgumentNullException("webvttContent");
+
+            String srtResult = webvttContent;
+
+            Int32 srtPartLineNumber = 0;
+
+            srtResult = Regex.Replace(srtResult, @"(WEBVTT\s+)(\d{2}:)", "$2"); // Removes 'WEBVTT' word
+
+            srtResult = Regex.Replace(srtResult, @"(\d{2}:\d{2}:\d{2})\.(\d{3}\s+)-->(\s+\d{2}:\d{2}:\d{2})\.(\d{3}\s*)", match =>
+            {
+                srtPartLineNumber++;
+                return srtPartLineNumber.ToString() + Environment.NewLine +
+                    Regex.Replace(match.Value, @"(\d{2}:\d{2}:\d{2})\.(\d{3}\s+)-->(\s+\d{2}:\d{2}:\d{2})\.(\d{3}\s*)", "$1,$2-->$3,$4");
+                // Writes '00:00:19.620' instead of '00:00:19,620'
+            }); // Writes Srt section numbers for each section
+
+            return srtResult;
+        }
+    }
