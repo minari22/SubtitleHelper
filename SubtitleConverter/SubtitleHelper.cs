16e653f35e7eff97d34dd86cfaf2cb9917fd8b76
AsValeO <theyhunger@mail.ru>
2015-10-29 12:02:44 +0000

    SubtitleHelper.cs references added, refactored

--- SubtitleConverter/SubtitleHelper.cs
+++ SubtitleConverter/SubtitleHelper.cs
@@ -1,29 +1,37 @@
-public static class SubtitleHelper
+using System;
+using System.Text.RegularExpressions;
+
+namespace SubtitleConverter
+{
+    public static class SubtitleHelper
     {
         /// <summary>
-        /// Convert WebVTT subtitles to Srt subtitles
+        ///     Convert WebVTT subtitles to Srt subtitles
         /// </summary>
         /// <param name="webvttContent">WebVTT string</param>
         /// <returns>SRT result</returns>
-        public static String ConvertWebvttToSrt(String webvttContent)
+        public static string ConvertWebvttToSrt(string webvttContent)
         {
             if (webvttContent == null)
-                throw new ArgumentNullException("webvttContent");
+                throw new ArgumentNullException(nameof(webvttContent));
 
-            String srtResult = webvttContent;
+            var srtResult = webvttContent;
 
-            Int32 srtPartLineNumber = 0;
+            var srtPartLineNumber = 0;
 
             srtResult = Regex.Replace(srtResult, @"(WEBVTT\s+)(\d{2}:)", "$2"); // Removes 'WEBVTT' word
 
-            srtResult = Regex.Replace(srtResult, @"(\d{2}:\d{2}:\d{2})\.(\d{3}\s+)-->(\s+\d{2}:\d{2}:\d{2})\.(\d{3}\s*)", match =>
-            {
-                srtPartLineNumber++;
-                return srtPartLineNumber.ToString() + Environment.NewLine +
-                    Regex.Replace(match.Value, @"(\d{2}:\d{2}:\d{2})\.(\d{3}\s+)-->(\s+\d{2}:\d{2}:\d{2})\.(\d{3}\s*)", "$1,$2-->$3,$4");
-                // Writes '00:00:19.620' instead of '00:00:19,620'
-            }); // Writes Srt section numbers for each section
+            srtResult = Regex.Replace(srtResult, @"(\d{2}:\d{2}:\d{2})\.(\d{3}\s+)-->(\s+\d{2}:\d{2}:\d{2})\.(\d{3}\s*)",
+                match =>
+                {
+                    srtPartLineNumber++;
+                    return srtPartLineNumber.ToString() + Environment.NewLine +
+                           Regex.Replace(match.Value,
+                               @"(\d{2}:\d{2}:\d{2})\.(\d{3}\s+)-->(\s+\d{2}:\d{2}:\d{2})\.(\d{3}\s*)", "$1,$2-->$3,$4");
+                    // Writes '00:00:19.620' instead of '00:00:19,620'
+                }); // Writes Srt section numbers for each section
 
             return srtResult;
         }
     }
+}
